d7b5b6208562a71602684dfe53638b9bebd34b31
AlexFlowers <>
2023-03-24 06:58:41 +0300

    some imports exports done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components'
 export default function App () {
   return (
-    <div>App.</div>
+    <BrowserRouter>
+      <Box sx={{backgrounColor: '#000' }}>
+        <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Feed />}/>
+        <Route path='/video/:id' element={<VideoDetail />}/>
+        <Route path='/search/:searchTerm' element={<ChannelDetail />}/>
+        <Route path='/search/:searchTerm' element={<SearchFeed />}/>
+      </Routes>
+      </Box>   
+    </BrowserRouter>
   )
 }
